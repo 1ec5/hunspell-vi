8ae4cb237cdd00242deedd24616a8d1e8cf42ffe
Minh Nguyen <mxn@1ec5.org>
2008-05-27 01:15:28 +0000

    Moved copyright and license notices into Subversion properties; removed unnecessary install.js

--- install.js
+++
@@ -1,9 +0,0 @@
-var err = initInstall("Vietnamese Dictionary", "dictionary@vi.mozdev.org", "1.0");
-if (err != SUCCESS) cancelInstall();
-
-var fProgram = getFolder("Program");
-err = addDirectory("", "dictionary@vi.mozdev.org", "dictionaries", fProgram,
-				   "dictionaries", true);
-if (err != SUCCESS) cancelInstall();
-
-performInstall();
