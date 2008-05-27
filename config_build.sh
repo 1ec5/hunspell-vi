b2ba44a46d6b46ec710d62bac232609800853cf0
Minh Nguyen <mxn@1ec5.org>
2008-05-27 01:01:43 +0000

    Imported Vietnamese Dictionary; currently works in Mozilla Firefox 3.0 RC1; listed as "Vietnamese (new)"

---
+++ config_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Build config for build.sh
+APP_NAME=vi_dictionary
+CHROME_PROVIDERS=
+CLEAN_UP=1
+ROOT_FILES=
+ROOT_DIRS="dictionaries"
+#'*CVS*'
+PRUNE_DIRS="*.svn*"
+BEFORE_BUILD=
+AFTER_BUILD=
