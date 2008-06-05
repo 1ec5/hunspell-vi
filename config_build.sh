50ae54f92231f2d6d3d368e2e1de7957bc554d4c
Minh Nguyen <mxn@1ec5.org>
2008-06-05 04:22:11 +0000

    Added install.js to the build script

--- config_build.sh
+++ config_build.sh
@@ -5,7 +5,7 @@ CHROME_PROVIDERS=
 CLEAN_UP=1
 ROOT_FILES="CHANGELOG LICENSE gpl.txt"
 ROOT_DIRS="dictionaries"
-VAR_FILES="install.rdf CHANGELOG"
+VAR_FILES="install.rdf install.js CHANGELOG"
 VERSION="1.0.1.$REV_NUM"
 #'*CVS*'
 PRUNE_DIRS="*.svn*"
