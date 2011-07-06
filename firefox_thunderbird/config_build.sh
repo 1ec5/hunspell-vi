581bfa43b586ba31994e7d9989a1d6d0a4cfdfba
Minh Nguyen <mxn@1ec5.org>
2011-07-06 17:04:13 +0000

    Released version 2.0.3

--- firefox_thunderbird/config_build.sh
+++ firefox_thunderbird/config_build.sh
@@ -6,7 +6,7 @@ CLEAN_UP=0
 ROOT_FILES="CHANGELOG LICENSE gpl.txt"
 ROOT_DIRS="../dictionaries"
 VAR_FILES="install.rdf install.js CHANGELOG"
-VERSION="2.0.2.$REV_NUM"
+VERSION="2.0.3.$REV_NUM"
 #'*CVS*'
 PRUNE_DIRS="*.svn*"
 BEFORE_BUILD=
