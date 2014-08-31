e5fbb06cacd6486cf74069d4a680855e31f9fca0
Minh Nguyen <mxn@1ec5.org>
2014-08-31 20:52:15 +0000

    Normalize to Vietnamese letters by default Added -a or --ascii flag for old behavior of stripping all diacritics

--- tools/normalize.py
+++ tools/normalize.py
@@ -9,13 +9,25 @@
 
 import sys, re
 
-REPLS = {u"[àảãáạăằẳẵắặâầẩẫấậ]": "a",   u"[ÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ]": "A",
-         u"đ": "d",                     u"Đ": "D",
-         u"[èẻẽéẹêềểễếệ]": "e",         u"[ÈẺẼÉẸÊỀỂỄẾỆ]": "E",
-         u"[ìỉĩíị]": "i",               u"[ÌỈĨÍỊ]": "I",
-         u"[òỏõóọôồổỗốộơờởỡớợ]": "o",   u"[ÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ]": "O",
-         u"[ùủũúụưừửữứự]": "u",         u"[ÙỦŨÚỤƯỪỬỮỨỰ]": "U",
-         u"[ỳỷỹýỵ]": "y",               u"[ỲỶỸÝỴ]": "Y"}
+ASCII_REPLS = {u"[àảãáạăằẳẵắặâầẩẫấậ]": "a", u"[ÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ]": "A",
+               u"đ": "d",                   u"Đ": "D",
+               u"[èẻẽéẹêềểễếệ]": "e",       u"[ÈẺẼÉẸÊỀỂỄẾỆ]": "E",
+               u"[ìỉĩíị]": "i",             u"[ÌỈĨÍỊ]": "I",
+               u"[òỏõóọôồổỗốộơờởỡớợ]": "o", u"[ÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ]": "O",
+               u"[ùủũúụưừửữứự]": "u",       u"[ÙỦŨÚỤƯỪỬỮỨỰ]": "U",
+               u"[ỳỷỹýỵ]": "y",             u"[ỲỶỸÝỴ]": "Y"}
+VIET_REPLS = {u"[àảãáạ]": u"a",     u"[ÀẢÃÁẠ]": u"A",
+              u"[ăằẳẵắặ]": u"ă",    u"[ĂẰẲẴẮẶ]": u"Ă",
+              u"[âầẩẫấậ]": u"â",    u"[ÂẦẨẪẤẬ]": u"Â",
+              u"[èẻẽéẹ]": u"e",     u"[ÈẺẼÉẸ]": u"E",
+              u"[êềểễếệ]": u"ê",    u"[ÊỀỂỄẾỆ]": u"Ê",
+              u"[ìỉĩíị]": u"i",     u"[ÌỈĨÍỊ]": u"I",
+              u"[òỏõóọ]": u"o",     u"[ÒỎÕÓỌ]": u"O",
+              u"[ôồổỗốộ]": u"ô",    u"[ÔỒỔỖỐỘ]": u"Ô",
+              u"[ơờởỡớợ]": u"ơ",    u"[ƠỜỞỠỚỢ]": u"Ơ",
+              u"[ùủũúụ]": u"u",     u"[ÙỦŨÚỤ]": u"U",
+              u"[ưừửữứự]": u"ư",    u"[ƯỪỬỮỨỰ]": u"Ư",
+              u"[ỳỷỹýỵ]": u"y",     u"[ỲỶỸÝỴ]": u"Y"}
 
 def print_help():
     """Prints help information to the command line."""
@@ -29,6 +41,8 @@ Available options:
   -f, --file FILE           Insert the contents of the specified plain text
                             file.
   -h, --help                Display this help message.
+  -a, --ascii               Remove all diacritics from the text, not just tone
+                            marks.
   -w, --letters,
       --letters-only        Remove all numbers, whitespace, and other characters
                             not in the Vietnamese alphabet.
@@ -43,10 +57,11 @@ generate letter frequencies on that file using the following sequence:
 """
 
 def main():
-    src = ""
+    src = []
     charset = "utf8"
     ignore_case = False
     skip_punct = False
+    is_ascii = False
     
     is_file = False
     is_charset = False
@@ -57,16 +72,21 @@ def main():
         if is_file:
             is_file = False
             f = file(arg, "r")
-            src += unicode("\n".join(f.readlines()), charset)
+            src += [unicode(line, charset) for line in f.readlines()]
             f.close()
             continue
         
-        # Character encoding
+        # Input character encoding
         if arg in ["--charset"]:
             is_charset = True
             continue
         if is_charset:
             charset = arg
+            continue
+        
+        # Output character encoding
+        if arg in ["-a", "--ascii"]:
+            is_ascii = True
             continue
         
         # Case insensitive
@@ -86,13 +106,17 @@ def main():
         
         src += unicode(arg, charset) + " "
     
-    for repl in REPLS.iteritems():
-        src = re.sub(repl[0], repl[1], src)
+    repls = ASCII_REPLS if is_ascii else VIET_REPLS
+    for regex, repl in repls.iteritems():
+        src = [re.sub(regex, repl, line, flags=re.U) for line in src]
     if ignore_case:
-        src = src.lower()
+        src = [line.lower() for line in src]
     if skip_punct:
-        src = re.sub("[\W\d]+", "", src)
-    print src
+        src = [re.sub(r"[\W\d]+", "", line, flags=re.U) for line in src]
+        sys.stdout.write("".join(src).encode("utf-8") + "\n")
+    else:
+        for line in src:
+            print line.encode("utf-8"),
 
 if __name__ == "__main__":
     main()
